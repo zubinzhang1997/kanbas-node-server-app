706cb33262f030e1816a2377c07b543d58010674
Zubin Zhang <zubinzhang@MacBook-Pro-3.local>
2024-04-04 19:47:19 -0400

    second commit

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Hello from "./Hello.js"
 import Lab5 from "./Lab5.js";
 import cors from "cors";
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
 
